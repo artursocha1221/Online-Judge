e2ec9418818bc3eec43740d87b512df944431153
Artur Socha <achtung4@op.pl>
2022-06-23 20:46:45 +0200

    Creating ProblemDto class

---
+++ Online-Judge/src/main/java/com/example/Online/Judge/dtos/ProblemDto.java
@@ -0,0 +1,13 @@
+package com.example.Online.Judge.dtos;
+
+public class ProblemDto {
+    private String statement;
+    private Long userId;
+
+    public String getStatement() {
+        return statement;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+}
